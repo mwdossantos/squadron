f6bc2664621f35b5d33388d2a89ea2311876c197
Matthew <matthew.martijn.wilson@gmail.com>
2018-12-19 16:25:09 +0100

    fix

--- js/script.js
+++ js/script.js
@@ -2,28 +2,49 @@
    // your page initialization code here
    // the DOM will be available here
    animator();
+
+   $(window).scroll(function() {
+
+    //After scrolling 100px from the top...
+    if ( $(window).scrollTop() >= 1 ) {
+        // $('header').css('transition', 'all .2s ease');
+        $('header').css('backgroundColor', 'var(--secondary)');
+
+    //Otherwise remove inline styles and thereby revert to original stying
+    } else {
+        $('header').attr('style', '');
+
+    }
+  });
+
 })();
 
 function animator() {
 
   ScrollReveal().reveal('header', {
-    delay: 150,
+    delay: 300,
     origin: 'top',
     distance: '100px',
-    opacity: 1
+    opacity: 0
   });
-  
-  ScrollReveal().reveal('main', {
-    delay: 150,
+
+  ScrollReveal().reveal('.introduction', {
+    delay: 300,
     origin: 'top',
-    distance: '180px',
-    opacity: 1
+    distance: '100px',
+    opacity: 0
   });
 
   ScrollReveal().reveal('.header-container', {
-    delay: 300,
+    delay: 600,
     origin: 'top',
-    distance: '20px',
+    distance: '100px',
+    opacity: 0
+  });
+  ScrollReveal().reveal('.introduction-left-container', {
+    delay: 600,
+    origin: 'left',
+    distance: '50px',
     opacity: 0
   });
 
