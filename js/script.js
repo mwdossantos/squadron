37a3bab76bfdd18a4a901075d9741597945b0073
Matthew <matthew.martijn.wilson@gmail.com>
2019-01-30 12:33:56 +0100

    fix

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 (function() {
    // your page initialization code here
    // the DOM will be available here
+   $(".pageclip-form__success__message").html("Bedankt! Het Squadron neemt zo snel mogelijk contact met je op 🚀");
    animator();
    other();
 
@@ -8,30 +9,43 @@
 
 function animator() {
 
-  // ScrollReveal().reveal('header', {
-  //   delay: 300,
-  //   origin: 'top',
-  //   distance: '100px',
-  //   opacity: 0
-  // });
+  ScrollReveal().reveal('header', {
+    delay: 200,
+    origin: 'top',
+    distance: '100px',
+    opacity: 0
+  });
+
+  ScrollReveal().reveal('section', {
+    delay: 200,
+    origin: 'bottom',
+    distance: '50px',
+    opacity: 0,
+  });
+
+  ScrollReveal().reveal('p, form, h1, h2, h3, a, .price-container, input, button', {
+    origin: 'bottom',
+    distance: '25px',
+    opacity: 0,
+    delay: 350,
+  });
+
+  ScrollReveal().reveal('footer', {
+    delay: 200,
+    origin: 'bottom',
+    distance: '100px',
+    opacity: 0,
+  });
 
 }
 
 function other() {
   $(".contact-container").click(function() {
     navigator("#contact","_self");
-  })
-
-  $(".contact-form").submit(function () {
-    sendContactForm();
-    return false;
   });
+
 }
 
-function sendContactForm() {
-  $(".message-sent").slideDown("slow");
-  setTimeout('$(".message-sent").slideUp();', 4000);
-}
 
 function navigator(location,name) {
   window.open(location,name);
